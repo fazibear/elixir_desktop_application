01309accf1e5905a7e8fa303c3b17fe331163eb2
Michał Kalbarczyk <fazibear@gmail.com>
2019-09-11 13:07:30 +0200

    Fix build.sh

--- build.sh
+++ build.sh
@@ -17,7 +17,7 @@ mkdir -p $APP_DIR
 echo "cp -r $RELEASE $APP_DIR"
 cp -r $RELEASE $APP_DIR
 echo "#!/bin/bash" > $APP_DIR/$APP
-echo 'DIR="\$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )' >> $APP_DIR/$APP
+echo 'DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"' >> $APP_DIR/$APP
 echo '$DIR/elixir_desktop_application/bin/elixir_desktop_application start' >> $APP_DIR/$APP
 chmod +x $APP_DIR/$APP
 
